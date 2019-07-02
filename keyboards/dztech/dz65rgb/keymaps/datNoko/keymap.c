b7da3c99fbfac5c6a1b782982f0de55b84dbe7ff
Sepp Niedermann <darkangel8500@gmx.de>
2019-07-02 22:29:06 +0200

    added some descriptions for my defined keys

--- keyboards/dztech/dz65rgb/keymaps/datNoko/keymap.c
+++ keyboards/dztech/dz65rgb/keymaps/datNoko/keymap.c
@@ -3,20 +3,16 @@
 #define _LAYER1 1
 #define _LAYER2 2
 #define _LAYER3 3
-#define TAPPING_TOGGLE 3
-#define CT_L_ANGLE LSFT(KC_NUBS)
-//#define GRALT_PIPE RALT(KC_NUBS)
-#define ISO_BSLA RALT(KC_MINS)
-#define ISO_LBRAC RALT(KC_8)
-#define ISO_RBRAC RALT(KC_9)
-#define ISO_SLA LSFT(KC_7)
-#define ISO_SWLBA RALT(KC_7)
-#define ISO_SWRBA RALT(KC_0)
-#define ISO_QUOT LSFT(KC_BSLASH)
-#define ISO_COMM LSFT(KC_COMM)
-#define SwirlLBRAC RALT(KC_7)
-#define SwirlRBRAC RALT(KC_0)
-#define ISO_BAR RALT(KC_NUBS)
+#define CT_L_ANGLE LSFT(KC_NUBS)	//this symbol > //
+#define ISO_BSLA RALT(KC_MINS)		//this symbol \ //
+#define ISO_LBRAC RALT(KC_8)		//this symbol [ //
+#define ISO_RBRAC RALT(KC_9)		//this symbol ] //
+#define ISO_SLA LSFT(KC_7)		//just a slash // 
+#define ISO_QUOT LSFT(KC_BSLASH)	//this symbol ' //
+#define ISO_COMM LSFT(KC_COMM)		//just a semicolon because fuck german ISO//
+#define SwirlLBRAC RALT(KC_7)		//this symbol { //
+#define SwirlRBRAC RALT(KC_0)		//this symbol } //
+#define ISO_BAR RALT(KC_NUBS)		//this symbol | //
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		[_LAYER0] = LAYOUT_65_ansi( /* Base */
@@ -27,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		    	KC_LCTL,       KC_LGUI,  KC_LALT,                KC_SPC,                KC_RALT, MO(1),    KC_RCTL,    KC_LEFT,           KC_DOWN,   KC_RIGHT),
 		[_LAYER1] = LAYOUT_65_ansi( /* FN */
 			KC_GESC,       KC_F1,    KC_F2,   KC_F3,  KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  KC_DEL,   KC_HOME,\
-		    	KC_TRNS,       RGB_TOG,  RGB_MOD, RGB_HUI,RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_TRNS, KC_PSCR, ISO_SWLBA,  ISO_SWRBA, RESET,    KC_PGUP,\
+		    	KC_TRNS,       RGB_TOG,  RGB_MOD, RGB_HUI,RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_TRNS, KC_PSCR, KC_LBRC,  KC_RBRC, RESET,    KC_PGUP,\
 			MO(0),RGB_SPI, RGB_SPD, KC_TRNS,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,          EEP_RST,  KC_PGDN,\
 		    	KC_LSFT,       KC_TRNS,  RGB_M_P, RGB_M_G,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,          KC_VOLU,  KC_MUTE,\
 		    	KC_TRNS,       KC_TRNS,  KC_TRNS,                  KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS,  KC_MPRV,          KC_VOLD,  KC_MNXT),
